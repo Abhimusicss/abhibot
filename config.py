f65571bd19ffb048388004eff8e7496e56ba1ace
Abhimusicss <tg050679@gmail.com>
2025-03-22 22:44:24 +0530

    Moon 🌙

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ISTKHAR-king/SpotifyxMusic",
+    "https://github.com/Abhimusicss/abhibot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "stable")
 GIT_TOKEN = getenv(
